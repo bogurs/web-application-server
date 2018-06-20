646f624ff8f6ba50aec86922284c3937cd8184f7
bogurs <thsong10@gmail.com>
2018-06-20 16:37:27 +0900

    p96 요구사항1 구현(index.html 반환하기)

---
+++ src/main/java/util/StringUtils.java
@@ -0,0 +1,7 @@
+package util;
+
+public class StringUtils {
+	public static String[] split_by_space(String line) {
+		return line.split(" ");
+	}
+}
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,20 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import util.StringUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,8 +31,27 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	BufferedReader bReader = new BufferedReader(new InputStreamReader(in));
+        	String line;
+        	StringBuffer allLine = new StringBuffer();
+        	byte[] body = null;
+        	
+        	while(!"".equals(line=bReader.readLine())){
+        		allLine.append(line);
+        		// null 인 경우 while 빠져나가게 하기 (무한루프에 빠질 수 있다)
+        		if(line==null){
+        			break;
+        		}
+        	}
+        	//line을 공백 단위로 자르기
+    		String[] tokens = StringUtils.split_by_space(allLine.toString());
+    		
+    		//요청 url 저장
+    		String url = tokens[1];
+    		
+    		body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        	
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello Song".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
